d1e92703370a755f44dfee048bb3d5719d17f819
Huang Jian-Min <yfr.huang@hotmail.com>
2021-01-13 19:04:28 +0800

    extract get shipper

--- src/main/kotlin/Cart.kt
+++ src/main/kotlin/Cart.kt
@@ -1,22 +1,24 @@
 class Cart {
     fun shippingFee(shipperName: String, product: Product): Double {
+        return getShipper(shipperName).calculateFee(product)
+    }
+
+    private fun getShipper(shipperName: String): Shipper {
         var shipper: Shipper
         when (shipperName) {
             "black cat" -> {
                 shipper = BlackCat()
-                return shipper.calculateFee(product)
             }
             "hsinchu" -> {
                 shipper = Hsinchu()
-                return shipper.calculateFee(product)
             }
             "post office" -> {
                 shipper = PostOffice()
-                return shipper.calculateFee(product)
             }
             else -> {
                 throw IllegalArgumentException("shipper not exist")
             }
         }
+        return shipper
     }
 }

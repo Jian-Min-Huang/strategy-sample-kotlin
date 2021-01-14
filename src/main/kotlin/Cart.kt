720f7f3d08883e3251c38bc1677546d1e8d6155e
Huang Jian-Min <yfr.huang@hotmail.com>
2021-01-14 10:34:32 +0800

    inline method and processing null check

--- src/main/kotlin/Cart.kt
+++ src/main/kotlin/Cart.kt
@@ -1,15 +1,11 @@
 class Cart {
-    var shippers = mapOf<String, Shipper>(
+    var shippers = mapOf(
         "black cat" to BlackCat(),
         "hsinchu" to Hsinchu(),
         "post office" to PostOffice(),
     )
 
     fun shippingFee(shipperName: String, product: Product): Double {
-        return getShipper(shipperName)!!.calculateFee(product)
-    }
-
-    private fun getShipper(shipperName: String): Shipper? {
-        return shippers[shipperName]
+        return shippers[shipperName]?.calculateFee(product) ?: error("not such shipper name : $shipperName")
     }
 }
